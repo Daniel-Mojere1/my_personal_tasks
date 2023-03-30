0c2d4fac2db73e90f53548a972422609f22771f3
Danie-Mojere1 <mojereabioladaniel@gmail.com>
2023-03-30 04:16:06 +0100

    An array and a loop

---
+++ c-programming/array_loop.c
@@ -0,0 +1,22 @@
+/*Create a program that computes the average mark of a stuudent.
+    - Create an array that stores the mark of 5 subjects.
+    - Compute the total marks by adding all the marks
+    - Divide the total marks by the total number of subjects
+    - Print the average marks*/
+#include <stdio.h>
+
+int main() {
+    int subjectMark[5];
+    int product;
+    float average;
+    printf("Enter 5 input values: ");
+    
+    for (int i=0; i<5; i++) {
+        scanf("%d", &subjectMark[i]);
+    }
+    product = subjectMark[0] + subjectMark[1] + subjectMark[2] + subjectMark[3] + subjectMark[4];
+    average = product/5;
+    printf("%.2f", average);
+
+    return 0;
+}
